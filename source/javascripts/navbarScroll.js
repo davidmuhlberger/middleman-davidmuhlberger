6cc37f8eb303e90b2cb405c8e28b14e60d50e5f6
david muhlberger <david.muhlberger@gmail.com>
2015-06-09 16:30:28 +0200

    solved

---
+++ source/javascripts/navbarScroll.js
@@ -0,0 +1,28 @@
+$(document).ready(function() {
+  $(window).scroll(function(e){
+    if ($(this).scrollTop() > 0) {
+      $(".navbar").css({
+        "background": "rgba(0, 0, 0, 0.7)",
+        "box-shadow": "0 0 2px black"
+      });
+    }
+    else {
+      $(".navbar").css({
+        "background": "transparent",
+        "box-shadow": "0 0 0px transparent"
+      });
+    }
+  });
+     if ($(this).scrollTop() > 0) {
+      $(".navbar").css({
+        "background": "rgba(0, 0, 0, 0.7)",
+        "box-shadow": "0 0 2px black"
+      });
+    }
+    else {
+      $(".navbar").css({
+        "background": "transparent",
+        "box-shadow": "0 0 0px transparent"
+    });
+  }
+});
